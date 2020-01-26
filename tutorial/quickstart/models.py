336e4b97617eceedf1d1eb130dc279d11e99dec5
peterhoward42 <peterhoward42@gmail.com>
2020-01-26 21:17:37 +0000

    Create Invoice model

--- tutorial/quickstart/models.py
+++ tutorial/quickstart/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Invoice(models.Model):
+    date = models.DateTimeField()
+    amount = models.DecimalField(decimal_places=2, max_digits=6)
